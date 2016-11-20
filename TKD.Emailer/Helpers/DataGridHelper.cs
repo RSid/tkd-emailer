e7ad9e70887fc9ca5c05e21957f27321a4fa59df
Alla Hoffman <alla.hoffman@buildium.com>
2016-11-20 16:58:24 -0500

    helper to get datagridview cell values by column name

---
+++ TKD.Emailer/Helpers/DataGridHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+
+namespace TKD.Emailer.Helpers
+{
+    public static class DataGridHelper
+    {
+        /// <summary>
+        /// From http://stackoverflow.com/questions/13436553/how-to-get-cell-value-of-datagridview-by-column-name
+        /// cred Patrick Quirk
+        /// </summary>
+        /// <param name="CellCollection"></param>
+        /// <param name="HeaderText"></param>
+        /// <returns></returns>
+        public static object GetCellValueFromColumnHeader(this DataGridViewCellCollection CellCollection, string HeaderText)
+        {
+            return CellCollection.Cast<DataGridViewCell>().First(c => c.OwningColumn.HeaderText == HeaderText).Value;
+        }
+    }
+}
