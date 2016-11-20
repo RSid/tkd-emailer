e4f2c332374fdeb210faf01c387a2ae23244fda6
Alla Hoffman <alla.hoffman@buildium.com>
2016-11-20 16:58:36 -0500

    EmailRecipientDTO

---
+++ TKD.Emailer/Models/EmailRecipientDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace TKD.Emailer.Models
+{
+    internal class EmailRecipientDTO
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Email { get; }
+
+
+        public EmailRecipientDTO(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
